b6f623744e144c7329e3e394e8f4f529b69bf9b8
TimZ <huai_1989@yeah.net>
2015-12-22 17:04:32 +0800

    Initial Commit

---
+++ TZRouteSearchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TZRouteSearchDemo
+//
+//  Created by iMac on 15/12/22.
+//  Copyright © 2015年 TZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
